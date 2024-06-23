04031ddc1827c4c9d38f980c26fcc3e00c55eac6
lauramufic <lauramufic23@gmail.com>
2024-06-23 16:49:26 +0200

    quicknotes

---
+++ app/src/main/java/com/example/quicknotesla/LoginActivity.java
@@ -0,0 +1,49 @@
+package com.example.quicknotesla;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText edUsername, edPassword;
+    Button btn;
+    TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        edUsername = findViewById(R.id.editTextLoginUsername);
+        edPassword = findViewById(R.id.editTextLoginPassword);
+        btn = findViewById(R.id.buttonLogin);
+        tv = findViewById(R.id.textViewNoviKorisnik);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = edUsername.getText().toString();
+                String password = edPassword.getText().toString();
+                if(username.length()==0 || password.length()==0){
+                    Toast.makeText(getApplicationContext(), "Molimo ispunite sva polja.", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Uspješno ste prijavljeni.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
